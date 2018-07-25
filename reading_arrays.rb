300fb373095288637157cbeb595ac46333ddfa73
ziesop20 <sincerelysophie@hotmail.com>
2018-07-25 15:07:43 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,19 +9,23 @@ def fourth_student_by_index
   STUDENT_NAMES[3]
 end
 puts fourth_student_by_index
+
 def last_student_by_index
   STUDENT_NAMES[5] 
 end
+last_student_by_index
 
 def first_student_by_method
   STUDENT_NAMES.first
 end
+first_student_by_method
 
 def last_student_by_method
-  Student_names.last 
   STUDENT_NAMES.last
 end
+last_student_by_method
 
 def first_second_and_third_students
   STUDENT_NAMES[0..2]
 end
+first_second_and_third_students
